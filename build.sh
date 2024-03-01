d8e2891a531a2d1fcc2eb1ac2d0ed2ee7cb96c00
me <me>
2024-03-01 13:36:09 +0100

    reorganising

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc -o ./target/app.out `pkg-config --cflags gtk+-3.0` `pkg-config --cflags gtk-vnc-2.0` ./src/*.c `pkg-config --libs gtk+-3.0` `pkg-config --libs gtk-vnc-2.0` -Wno-deprecated-declarations
-cp -r ./src/images ./src/icons ./src/ui.glade ./target
+cp -r ./glade/images ./glade/icons ./glade/ui.glade ./target
