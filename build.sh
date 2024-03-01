8d6a972e9cc5788484c259fb3fc30cbaa6dd4b1e
me <me>
2024-03-01 13:41:44 +0100

    some simplifications

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+TARGET_DIRECTORY="target"
+
+if [ ! -d "$TARGET_DIRECTORY" ]; then
+mkdir ./$TARGET_DIRECTORY
+fi
+
+echo "Building VNC Client..."
+
 gcc -o ./target/app.out `pkg-config --cflags gtk+-3.0` `pkg-config --cflags gtk-vnc-2.0` ./src/*.c `pkg-config --libs gtk+-3.0` `pkg-config --libs gtk-vnc-2.0` -Wno-deprecated-declarations
-cp -r ./glade/images ./glade/icons ./glade/ui.glade ./target
+cp -r ./glade/images ./glade/icons ./glade/ui.glade ./$TARGET_DIRECTORY
+
+echo "Done."
